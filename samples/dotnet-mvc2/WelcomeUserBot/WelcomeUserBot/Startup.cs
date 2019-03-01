0f36f9dfa4be18797a04ef750f80c530d503614a
Dan Shelly <v-dashel@microsoft.com>
2019-03-01 13:52:07 -0800

    Updated Startup, added AdapterWithErrorHandler

--- samples/dotnet-mvc2/WelcomeUserBot/WelcomeUserBot/Startup.cs
+++ samples/dotnet-mvc2/WelcomeUserBot/WelcomeUserBot/Startup.cs
@@ -5,12 +5,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration;
+using Microsoft.Bot.Builder.BotFramework;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using System;
 using WelcomeUserBot.Bots;
 
 namespace WelcomeUserBot
@@ -29,11 +28,27 @@ namespace WelcomeUserBot
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Create the credential provider to be used with the Bot Framework Adapter.
+            services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
+
             // Add the Adapter as a singleton and our Bot as transient.
             services.AddSingleton<IBotFrameworkHttpAdapter>(sp => new BotFrameworkHttpAdapter());
 
+            // Create the Bot Framework Adapter with error handling enabled. 
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.) 
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            services.AddSingleton<ConversationState>();
+
             // Create conversation and user state with in-memory storage provider.
             IStorage storage = new MemoryStorage();
+
             // ConversationState conversationState = new ConversationState(storage);
             var userState = new UserState(storage);
 
