a4edabad2530fe9bf0fe9e153b5d4fe686073d77
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2019-05-14 14:25:24 -0700

    WIP: Issues report

---
+++ BotDocs_Tools/GitTools/ReportUtils/IssuesReport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Windows.Forms;
+
+namespace ReportUtils
+{
+    public class IssuesReport
+    {
+        public RichTextBox Status { get; private set; }
+        public FolderBrowserDialog OpenFolderDialog { get; private set; }
+        public SaveFileDialog SaveDialog { get; private set; }
+        public string Recents { get; }
+            = Environment.GetFolderPath(Environment.SpecialFolder.Recent);
+
+        public IssuesReport(
+            RichTextBox status,
+            FolderBrowserDialog openFolderDialog,
+            SaveFileDialog saveDialog)
+        {
+            Contract.Requires(Status != null);
+            Contract.Requires(OpenFolderDialog != null);
+            Contract.Requires(SaveDialog != null);
+
+            Status = status;
+            OpenFolderDialog = openFolderDialog;
+            SaveDialog = saveDialog;
+        }
+
+        public bool Run()
+        {
+            return false;
+            // Get local repo root?
+            // Extrapolate to on-line repo?
+            // Get issues (and filter?)
+            // Generate and save report.
+        }
+    }
+}
