7100dc1f3ec08625ed37352bf93cb2fc1eda5a1c
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2020-11-23 15:14:42 -0800

    Add Adaptive-proactive bot

---
+++ samples/AdaptiveWithProactiveAndCodeActions/01.multi-turn-prompt/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdaptiveScratchBot
+{
+    public static class StringExtensions
+    {
+        public static string Oxford(this IEnumerable<string> items, string separator, string conjunction)
+        {
+            if (items is null || !items.Any()) return string.Empty;
+
+            var list = items.ToList();
+            switch (list.Count)
+            {
+                case 1: return list[0];
+                case 2: return $"{list[0]} {conjunction} {list[1]}";
+                default:
+                    var sb = new StringBuilder();
+
+                    for (int i = 0; i<list.Count-1; i++)
+                    {
+                        sb.Append(list[i]);
+                        sb.Append(separator);
+                    }
+                    sb.Append(conjunction + " ");
+                    sb.Append(list.Last());
+
+                    return sb.ToString();
+            }
+        }
+    }
+}
